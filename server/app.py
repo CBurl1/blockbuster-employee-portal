d2fdac40a8f632bf476abed626c8583c974646f4
Mason Parks <kmparks60@gmail.com>
2023-04-18 03:12:47 -0400

    app.py

--- server/app.py
+++ server/app.py
@@ -3,16 +3,38 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 
 # Local imports
 from config import app, db, api
-from models import BlockBuster, Movie, Rental, Renter
+from models import Movie, Rental, Client
 
 # Views go here!
-
+class Movie(Resource):
+    def get(self):
+        m_list = []
+        for m in Movie.query.all():
+            m_dict = {
+                'name': m.name,
+                'cost': m.cost,
+                'rating': m.rating
+            }
+            m_list.append(m_dict)
+        return make_response(m_list, 200)
+    
+class Client(Resource):
+    def get(self):
+        c_list = []
+        for c in Client.query.all():
+            c_dict = {
+                'first_name': c.first_name,
+                'last_name': c.last_name,
+                'age': c.age
+            }
+            c_list.append(c_dict)
+        return make_response(c_list, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
