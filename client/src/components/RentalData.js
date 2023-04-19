2979e1402e788d8a60c4b4f99f3a1f76e5cd608a
Mason Parks <kmparks60@gmail.com>
2023-04-19 17:42:02 -0400

    updated RentalData table

--- client/src/components/RentalData.js
+++ client/src/components/RentalData.js
@@ -7,9 +7,10 @@ function RentalData({client_id, movie_id, id}) {
                 <table class='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
                 <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
                     <tr>
-                    <th scope='col' className="px-6 py-3">Rental Number</th>
-                    <th scope='col'className="px-6 py-3">Movies</th>
-                    <th scope='col' className="px-6 py-3">Client</th>
+                        <th scope='col' className="px-6 py-3">Rental Number</th>
+                        <th scope='col'className="px-6 py-3">Movies</th>
+                        <th scope='col' className="px-6 py-3">Client</th>
+                        <th scope='col' className="px-6 py-3">Edit Rental</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -18,7 +19,7 @@ function RentalData({client_id, movie_id, id}) {
                     <td className="px-6 py-4">{movie_id}</td>
                     <td className="px-6 py-4">{client_id}</td>
                     <td className="px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <a href="edit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                     </td>
                     </tr>
                 </tbody>
